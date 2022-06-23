aef781a572717e8131bd616a552a03b1eb69cbd0
Bhargavasai2005 <94349388+Bhargavasai2005@users.noreply.github.com>
2022-06-23 20:53:45 +0530

    Add files via upload

---
+++ phyton proj3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 23 20:27:20 2022
+
+@author: ADMIN
+"""
+
+list1=[12,-7,5,64,-14]
+for num in list1:
+    if num>=0:
+            print(num,end=" ")
+list2=[12,14,-95,3]
+for num in list2:
+    if num>=0:
+             print(num,end=" ")
